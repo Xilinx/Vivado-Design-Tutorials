5f6f050c0ad485ea56bd8aec37f671bb12c8a01e
Goutham Pocklassery <goutham.pocklassery@xilinx.com>
2021-07-12 11:40:58 -0600

    Update create_rp1_bdc.tcl

--- Device_Architecture_Tutorials/Versal/DFX/1RP_AXI_GPIO_in_RP_Interface_INI/scripts/create_rp1_bdc.tcl
+++ Device_Architecture_Tutorials/Versal/DFX/1RP_AXI_GPIO_in_RP_Interface_INI/scripts/create_rp1_bdc.tcl
@@ -1,3 +1,20 @@
+# #########################################################################
+#© Copyright 2021 Xilinx, Inc.
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+# ###########################################################################
+
+
 #Create a hierarchy for RP1
 group_bd_cells rp1 [get_bd_cells smartconnect_0] [get_bd_cells xlconstant_1] [get_bd_cells axi_gpio_1] [get_bd_cells axi_noc_1]
 #Create a hierarchy for static region
