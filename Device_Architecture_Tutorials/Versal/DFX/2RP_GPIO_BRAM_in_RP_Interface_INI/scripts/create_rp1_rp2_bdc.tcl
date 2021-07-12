04c7bc38c6c630546e7334f837a6dfdbc9f552e6
Goutham Pocklassery <goutham.pocklassery@xilinx.com>
2021-07-12 14:44:39 -0600

    added copyright headers

--- Device_Architecture_Tutorials/Versal/DFX/2RP_GPIO_BRAM_in_RP_Interface_INI/scripts/create_rp1_rp2_bdc.tcl
+++ Device_Architecture_Tutorials/Versal/DFX/2RP_GPIO_BRAM_in_RP_Interface_INI/scripts/create_rp1_rp2_bdc.tcl
@@ -1,3 +1,19 @@
+# #########################################################################
+#© Copyright 2021 Xilinx, Inc.
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+# ###########################################################################
+
 #Create a hierarchy for static region
 group_bd_cells static_region [get_bd_cells clk_wizard_0] [get_bd_cells axi_bram_ctrl_0] [get_bd_cells smartconnect_0] [get_bd_cells versal_cips_0] [get_bd_cells xlconstant_0] [get_bd_cells axi_gpio_0] [get_bd_cells proc_sys_reset_0] [get_bd_cells axi_noc_0] [get_bd_cells axi_bram_ctrl_0_bram]
 #Create a hierarchy for RP1
--- Device_Architecture_Tutorials/Versal/DFX/Update_BD_Boundary/scripts/enable_dfx_bdc.tcl
+++ Device_Architecture_Tutorials/Versal/DFX/Update_BD_Boundary/scripts/enable_dfx_bdc.tcl
@@ -1,5 +1,20 @@
-#set_param bd.enableDFX 1
-#set_property PR_FLOW 1 [current_project]
+# #########################################################################
+#© Copyright 2021 Xilinx, Inc.
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+# ###########################################################################
+
+
 current_bd_design [get_bd_designs design_1]
 #Enable DFX for the BDC rp1
 set_property -dict [list CONFIG.ENABLE_DFX {true}] [get_bd_cells rp1]
