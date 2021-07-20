5ab2466df93003526eb8ee8251cfc7434998a258
Goutham Pocklassery <goutham.pocklassery@xilinx.com>
2021-07-20 12:03:12 -0600

    removing 9th  tutorial

--- Device_Architecture_Tutorials/Ultrascale+/DFX/Abstract_Shell_Per_SLR_Compilation/scripts/initial_compile/run_all.tcl
+++
@@ -1,8 +0,0 @@
-
-set list_projs [get_projects -quiet]
-if { $list_projs eq "" } {
-   create_project project_1 ../../output/initial_compile -part xcvu13p-fhga2104-3-e -force
-}
-
-source create_ipi.tcl
-source run_impl.tcl
