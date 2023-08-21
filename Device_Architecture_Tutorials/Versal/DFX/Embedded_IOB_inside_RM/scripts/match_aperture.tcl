8b7f3983dd6ba7a9a30f240c383b2cfdb2f04357
Dye <davidd@amd.com>
2023-08-21 14:30:09 -0600

    Manual aperture matching

--- Device_Architecture_Tutorials/Versal/DFX/Embedded_IOB_inside_RM/scripts/match_aperture.tcl
+++ Device_Architecture_Tutorials/Versal/DFX/Embedded_IOB_inside_RM/scripts/match_aperture.tcl
@@ -1,5 +1,5 @@
 # #########################################################################
-#Â© Copyright 2021 Xilinx, Inc.
+#© Copyright 2021 Xilinx, Inc.
 
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
@@ -14,12 +14,16 @@
 #limitations under the License.
 # ###########################################################################
 
-#an issue exists in Vivado 2022.x regarding automatic address setting, requiring a revisit to the first RM
+#an issue exists in Vivado 2022.2 regarding automatic address setting, requiring a revisit to the first RM
 #this issue has been resolved in Vivado 2023.1
 #assign addresses in rp1rm2 for Vivado 2022.x
 current_bd_design [get_bd_designs rp1rm1]
 delete_bd_objs [get_bd_addr_segs] [get_bd_addr_segs -excluded]
 assign_bd_address
+set_property offset 0x80010000 [get_bd_addr_segs {S_AXI/SEG_axi_gpio_0_Reg}]
+set_property offset 0x80030000 [get_bd_addr_segs {S_AXI1/SEG_axi_gpio_1_Reg}]
+validate_bd_design
+save_bd_design
 
 
 #Upgrade the modified RP BDC at the top. Corresponds to clicking "Refresh Modules" banner at the top
