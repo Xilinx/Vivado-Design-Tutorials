ddc3b2374eeb5bba99e6026b0a0542a345d61a8f
Dye <davidd@amd.com>
2023-08-31 10:44:33 -0600

    repaired 2RP clock sharing design

--- Device_Architecture_Tutorials/Versal/DFX/2RPs_Sharing_ClockRegion/scripts/enable_dfx_bdc.tcl
+++ Device_Architecture_Tutorials/Versal/DFX/2RPs_Sharing_ClockRegion/scripts/enable_dfx_bdc.tcl
@@ -9,11 +9,9 @@ current_bd_design [get_bd_designs design_1]
 set_property -dict [list CONFIG.ENABLE_DFX {true}] [get_bd_cells {rp1 rp2}]
 #Lock the Static-RP interface
 set_property -dict [list CONFIG.LOCK_PROPAGATE {true}] [get_bd_cells {rp1 rp2}]
-#Set the Aperture for the interface ports of RP1
-#set_property APERTURES {0x0x000_8001_0000 64K} [get_bd_intf_pins /rp1/S_AXI]
-#set_property APERTURES {0x000_8003_0000 64K} [get_bd_intf_pins /rp2/S_AXI]
 
 ##Validate and Save the Top BD
-validate_bd_design
-save_bd_design
+#wait until after apertures have been aligned
+#validate_bd_design
+#save_bd_design
 
