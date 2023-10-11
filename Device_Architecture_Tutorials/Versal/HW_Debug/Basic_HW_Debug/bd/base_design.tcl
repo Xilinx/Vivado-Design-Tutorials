0ed9a040e7b9ed5ddc95bd4bb80c0207d0e7f399
Matt Piazza <mpiazza@amd.com>
2023-10-11 14:14:45 -0600

    Updated license headers

--- Device_Architecture_Tutorials/Versal/HW_Debug/Basic_HW_Debug/bd/base_design.tcl
+++ Device_Architecture_Tutorials/Versal/HW_Debug/Basic_HW_Debug/bd/base_design.tcl
@@ -1,3 +1,8 @@
+# #########################################################################
+# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+
+# SPDX-License-Identifier: MIT
+# ###########################################################################
 
 ################################################################
 # This is a generated script based on design: design_1
